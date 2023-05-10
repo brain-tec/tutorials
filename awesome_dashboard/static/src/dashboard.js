1a047dd3679af002fab7a8eb834f6c015343f5bf
Géry Debongnie <ged@odoo.com>
2023-05-10 22:06:54 +0200

    [SOLUTION] 2.7 Periodic update

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,4 +1,4 @@
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
@@ -11,10 +11,7 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action = useService("action");
-        const statisticService = useService("awesome_dashboard.statistics");
-        onWillStart(async () => {
-            this.statistics = await statisticService.loadStatistics();
-        });
+        this.statistics = useState(useService("awesome_dashboard.statistics"));
     }
 
     openCustomerView() {
