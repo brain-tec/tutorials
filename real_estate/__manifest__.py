b61223dba74611754e1c84e90f9f7ded9d89ac13
Antoine Vandevenne (anv) <anv@odoo.com>
2024-08-01 19:29:50 +0200

    chap 4 - many2one field

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -5,14 +5,16 @@
     'depends': ['base'],
     'data': [
         # Model data
-        'data/real_estate_property_data.xml',
+        'data/real_estate_property_type_data.xml',
+        'data/real_estate_property_data.xml',  # Depends on `real_estate_property_type_data.xml`
 
         # Security
         'security/ir.model.access.csv',
 
         # Views
+        'views/real_estate_property_type_views.xml',
         'views/real_estate_property_views.xml',
-        'views/menus.xml',  # Depends on `real_estate_property_views.xml`
+        'views/menus.xml',  # Depends on actions in views.
     ],
     'application': True,
 }
