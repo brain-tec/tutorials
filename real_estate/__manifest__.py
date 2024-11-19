rising-systems/oxp24-js-framework-tutorials

chap 4 - module structure

real_estate/__manifest__.py
@@ -4,11 +4,16 @@
     'license': 'LGPL-3',
     'depends': ['base'],
     'data': [
-        'actions.xml',
-        'ir.model.access.csv',
-        'menus.xml',  # Depends on `actions.xml`
-        'real_estate_property_data.xml',
-        'real_estate_property_views.xml',
+        # Model data
+        'data/real_estate_property_data.xml',
+
+        # Security
+        'security/ir.model.access.csv',
+
+        # Views
+        'views/actions.xml',
+        'views/menus.xml',  # Depends on `actions.xml`
+        'views/real_estate_property_views.xml',
     ],
     'application': True,
 }

brain-tec/tutorials

real_estate/__manifest__.py
@@ -4,11 +4,15 @@
     'license': 'LGPL-3',
     'depends': ['base'],
     'data': [
-        'actions.xml',
-        'ir.model.access.csv',
-        'menus.xml',  # Depends on `actions.xml`
-        'real_estate_property_data.xml',
-        'real_estate_property_views.xml',
+        # Model data
+        'data/real_estate_property_data.xml',
+
+        # Security
+        'security/ir.model.access.csv',
+
+        # Views
+        'views/real_estate_property_views.xml',
+        'views/menus.xml',  # Depends on `real_estate_property_views.xml`
     ],
     'application': True,
 }