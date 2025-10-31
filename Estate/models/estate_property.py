4da5a7b7f154631db114800b4b58a0c6743f0ac7
hapat-odoo <hapat@odoo.com>
2025-10-31 11:32:49 +0530

    [ADD]  estate:  Added a New Module estate_property
    Added a new module for estate property and completed tasks 1, 2, and 3.
     I added a models folder and learned to push data into PostgreSQL using ORM.

---
+++ Estate/models/estate_property.py
@@ -0,0 +1,35 @@
+#always check for odoo import from venv  due to which errors in psql might arise 
+#structure module name 
+
+#Models
+# init
+# module property - this consist of all files u wanna add and substract 
+#init 
+#manifest
+
+
+
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West'),
+    ])
